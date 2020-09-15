b2873ece9fd9ee5eef19757e55947d0750d30bdc
DevDreamSolution <q3construction1@gmail.com>
2020-09-15 11:23:11 +0800

    Initial

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+  use App\Http\Controllers\LanguageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+// Route url
+Route::get('/', 'DashboardController@dashboardAnalytics');
+
+// Route Dashboards
+Route::get('/dashboard-analytics', 'DashboardController@dashboardAnalytics');
+
+// Route Components
+Route::get('/sk-layout-2-columns', 'StaterkitController@columns_2');
+Route::get('/sk-layout-fixed-navbar', 'StaterkitController@fixed_navbar');
+Route::get('/sk-layout-floating-navbar', 'StaterkitController@floating_navbar');
+Route::get('/sk-layout-fixed', 'StaterkitController@fixed_layout');
+
+// acess controller
+Route::get('/access-control', 'AccessController@index');
+Route::get('/access-control/{roles}', 'AccessController@roles');
+Route::get('/modern-admin', 'AccessController@home')->middleware('permissions:approve-post');
+
+// Auth::routes();
+
+// locale Route
+Route::get('lang/{locale}',[LanguageController::class,'swap']);
