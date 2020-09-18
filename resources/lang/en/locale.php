87adbd6b2713d98bf7d1be38b43af76eef99d725
DevDreamSolution <q3construction1@gmail.com>
2020-09-18 21:59:15 +0800

    update en lang

--- resources/lang/en/locale.php
+++ resources/lang/en/locale.php
@@ -2,11 +2,75 @@
 
 return [
     "Dashboard" => "Dashboard",
-    "Starter kit" => "Starter kit",
-    "2 Columns" => "2 Columns",
-    "Fixed Navbar" => "Fixed Navbar",
-    "Floating Navbar" => "Floating Navbar",
-    "Fixed Layout" => "Fixed Layout",
-    "Raise Support" => "Raise Support",
-    "Documentation" => "Documentation"
+    "id" => "ID",
+    "CreatedAt" => "CreatedAt",
+    "UpdatedAt" => "UpdatedAt",
+    "Actions" => "Actions",
+    "enterEmail" => "Enter email address",
+    "enterPassword" => "Enter password",
+    "Email" => "Email",
+    "Password" => "Password",
+    "RememberMe" => "Remember Me",
+    "Name" => "Name",
+    "Surname" => "Surname",
+    "ConfirmPassword" => "Confirm Password",
+    "Register" => "Register",
+    "acceptCondition" => "I accept the terms & conditions.",
+    "CreateAccount" => "Create Account",
+    "fillCreateForm" => "Fill the below form to create a new account.",
+    "Login" => "Login",
+    "Username" => "Username",
+    "Logout" => "Logout",
+    "userPanel" => "User panel",
+    "adminPanel" => "Admin panel",
+    "role" => [
+        "title" => "Roles",
+        "create" => "Create Role",
+        "edit" => "Edit Role",
+        "update" => "Update Role",
+        "save" => "Save Role",
+        "list" => "Role List",
+        "name" => "Name",
+        "details" => "Role Details",
+        "permissions" => "Permissions",
+        "message" => [
+            "save" => "The role is added successfully.",
+            "update" => "The role is updated successfully.",
+            "delete" => "The role is deleted successfully."
+        ]
+    ],
+    "user" => [
+        "title" => "Users",
+        "create" => "Create User",
+        "edit" => "Edit User",
+        "update" => "Update User",
+        "save" => "Save User",
+        "list" => "User List",
+        "details" => "User Details",
+        "lock" => "Lock",
+        "message" => [
+            "save" => "The user is added successfully.",
+            "update" => "The user is updated successfully.",
+            "delete" => "The user is deleted successfully."
+        ],
+    ],
+    "profile" => [
+        "title" => "Profile",
+        "save" => "Save changes",
+        "edit" => "Edit Profile",
+        "general" => "General",
+        "changePassword" => "Change Password",
+        "upload" => "Upload new photo",
+        "allow" => "Allowed JPG, GIF or PNG. Max size of 800kB",
+        "updateSuccess" => "The profile is saved successfully.",
+        "oldPassword" => "Old Password",
+        "newPassword" => "New Password",
+        "retypePassword" => "Retype New Password"
+
+    ],
+    "error" => [
+        "lockTitle" => "Your account is locked!",
+        "lockDescription" => "Your account is locked by admin now, contact to admin.",
+        "notMatchPassword" => "The specified password does not match the database password."
+    ]
 ];
