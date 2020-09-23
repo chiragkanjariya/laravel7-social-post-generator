e1c217bc938a22a75a29084536b4f19df5764791
kcaribou <kcaribou0428@gmail.com>
2020-09-22 19:52:56 -0700

    integrate profile and post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
@@ -21,13 +23,26 @@ class PostController extends Controller
   ];
 
   public function manage_index(){
-    $profiles = array();
-    array_push($profiles, array('user'=> User::find(1), 'role'=> User::find(1)->roles->first()->name, 'id'=> 2, 'count'=> 1, 'niche'=> 'Sports', 'hashtags'=>'laravel, python, development', 'color'=> '#93e7f1'));
-    array_push($profiles, array('user'=> User::find(2), 'role'=> User::find(2)->roles->first()->name, 'id'=> 1, 'count'=> 2, 'niche'=> 'Model', 'hashtags'=>'shopify, python, web', 'color'=> '#c6e2d3'));
-    array_push($profiles, array('user'=> User::find(2), 'role'=> User::find(2)->roles->first()->name, 'id'=> 3, 'count'=> 3, 'niche'=> 'Science', 'hashtags'=>'marketing, wordpress, magento', 'color'=> '#c6e2d3'));
+    $result = array();
+    $profiles = DB::select(DB::raw('
+    SELECT profiles.*, posts_cnt.cnt, niches.name as niche_name FROM PROFILES
+      LEFT JOIN (SELECT COUNT(posts.id) AS cnt, posts.profile_id FROM posts WHERE DATE(posts.created_at) = \'2020-09-23\' GROUP BY posts.profile_id) AS posts_cnt ON profiles.id = posts_cnt.profile_id
+      LEFT JOIN niches ON profiles.niche_id = niches.id
+    ORDER BY posts_cnt.cnt'));
+    foreach ($profiles as $profile)
+    {
+      $user = User::find($profile->user_id);
+      $role = $user->roles->first()->name;
+      $id = $profile->id;
+      $count = $profile->cnt;
+      $niche = $profile->niche_name;
+      $hashtags = $profile->hashtag;
+      $color = $profile->favour_color;
+      array_push($result, array('user'=> $user, 'role'=> $role, 'id'=> $id, 'count'=> $count, 'niche'=> $niche, 'hashtags'=>$hashtags, 'color'=> $color));
+    }
     return view('pages.post-manage', [
       'pageConfigs' => $this->pageConfigs,
-      'profiles' => $profiles
+      'profiles' => $result
     ]);
   }
   /**
@@ -66,6 +81,7 @@ class PostController extends Controller
       return new JsonResponse("The image should be uploaded.", 203);
     }
     $post = Post::create( [ 'post_title' => $request->title,
+            'isoverlay' => $request->isoverlay,
             'post_content' => $request->content,
             'post_image' => $imagePath,
             'profile_id' => $request->profile_id,
