eb9e65229f1e57b1fd3e9591082464ccccc202a0
DevDreamSolution <q3construction1@gmail.com>
2020-09-29 09:14:54 +0800

    user and freepost relation

--- app/Models/Freepost.php
+++ app/Models/Freepost.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Freepost extends Model
 {
     protected $table = 'freeposts';
+
+    /**
+     * The user of freepost.
+     * 
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
