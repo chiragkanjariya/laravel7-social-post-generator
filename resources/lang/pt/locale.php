c549cc0e8cb8b8d8c5f0aeda6e263c9b38c1d95f
devdreamsolution <q3construction1@gmail.com>
2020-10-06 10:37:32 -0700

    add pt langauge files

--- resources/lang/pt/locale.php
+++ resources/lang/pt/locale.php
@@ -1,12 +1,159 @@
 <?php
 
 return [
-  "Dashboard" => "painel de controle",
-  "Starter kit" => "Kit iniciante",
-  "2 Columns" => "2 Colunas",
-  "Fixed Navbar" => "Fixed Navbar",
-  "Floating Navbar" => "Barra de navegação flutuante",
-  "Fixed Layout" => "Layout fixo",
-  "Raise Support" => "Levantar suporte",
-  "Documentation" => "Documentação"
+    "Dashboard" => "Como funciona",
+    "id" => "ID",
+    "CreatedAt" => "Criado em",
+    "UpdatedAt" => "Atualizado em",
+    "Actions" => "Ações",
+    "enterEmail" => "Insira o endereço de e-mail",
+    "enterPassword" => "Digite a senha",
+    "Email" => "Email",
+    "Password" => "Senha",
+    "RememberMe" => "Lembre de mim",
+    "Name" => "Nome",
+    "Surname" => "Sobrenome",
+    "ConfirmPassword" => "Confirme a senha",
+    "Register" => "Registrar",
+    "acceptCondition" => "Eu aceito os termos e condições.",
+    "fillCreateForm" => "Preencha o formulário abaixo para criar uma nova conta.",
+    "Login" => "Login",
+    "Username" => "Nome do usuário",
+    "Logout" => "Sair",
+    "userPanel" => "Painel do usuário",
+    "adminPanel" => "Painel de administração",
+    "approve" => "Aprovar",
+    "delete" => "Deletar",
+    "warning" => "Atenção",
+    "post-manage" => [
+        "title" => "Gerenciar post"
+    ],
+    "post-view" => [
+        "title" => "Novos posts"
+    ],
+    "mypost" => [
+        "title" => "Meu posts",
+        "message" => [
+            "delete" => "A postagem foi excluída com sucesso."
+        ]
+    ],
+    "freepost" => [
+        "title" => "postagens grátis",
+        "create" => "Crie uma postagem gratuita",
+        "save" => "Salvar postagem",
+        "content" => "Publicar conteúdo",
+        "message" => [
+            "save" => "A postagem gratuita foi armazenada com sucesso.",
+            "delete" => "A postagem gratuita foi removida com sucesso."
+        ]
+    ],
+    "instagram" => [
+        "title" => "Instagram",
+        "management" => "Gestão do Instagram",
+        "followers" => "Seguidores",
+        "bestHashtags" => "Melhores Hashtags",
+        "posts" => "Postagens",
+        "advices" => "Análise",
+        "save" => "Salvar Instagram",
+        "edit" => "Editar Instagram",
+        "message" => [
+            "save" => "Os dados do Instagram foram armazenados com sucesso.",
+            "delete" => "Os dados do Instagram foram apagados com sucesso."
+        ]
+    ],
+    "details" => "Detalhes",
+    "role" => [
+        "title" => "Funções",
+        "create" => "Criar função",
+        "edit" => "Editar função",
+        "update" => "Atualizar função",
+        "save" => "Salvar função",
+        "delete" => "Excluir função",
+        "list" => "Lista de funções",
+        "name" => "Nome",
+        "details" => "Detalhes da função",
+        "permissions" => "Permições",
+        "message" => [
+            "save" => "A função foi adicionada com sucesso.",
+            "update" => "A função foi atualizada com sucesso.",
+            "delete" => "A função foi excluída com sucesso."
+        ]
+    ],
+    "user" => [
+        "title" => "Gestão de usuários",
+        "create" => "Criar usuário",
+        "edit" => "Criar usuário",
+        "update" => "Atualizar usuário",
+        "save" => "Salvar usuário",
+        "delete" => "Deletar usuário",
+        "list" => "Lista de usuários",
+        "details" => "Detalhes do usuario",
+        "lock" => "Bloqueio",
+        "status" => [
+            "title" => "Status",
+            "activate" => "Ativar",
+            "deactivate" => "Desativar"
+        ],
+        "field" => [
+            "name" => "Nome",
+            "email" => "Email",
+            "role" => "Fun",
+            "password" => "Senha",
+            "confirm-password" => "Confirmar senha"
+        ],
+        "message" => [
+            "save" => "O usuário foi adicionado com sucesso.",
+            "update" => "O usuário foi atualizado com sucesso.",
+            "delete" => "O usuário foi excluído com sucesso."
+        ],
+    ],
+    "profile" => [
+        "title" => "Meu perfil",
+        "save" => "Salvar perfil",
+        "update" => "Atualizar perfil",
+        "edit" => "Editar Perfil",
+        "create" => "Criar perfil",
+        "niche" => "Nicho",
+        "hashtag" => "Hashtag",
+        "addTag" => "Adicione 3 ou mais tags...",
+        "favourColor" => "Cor favorita",
+        "instagram" => "Instagram",
+        "instagram_hold" => "Exemplo: https://www.instagram.com/user",
+        "message" => [
+            "delete" => " O perfil foi excluído com sucesso.",
+            "overProfile" => "A quantidade de perfis para o seu nível acabou."
+        ]
+    ],
+    "account" => [
+        'title' => "Minha conta",
+        "create" => "Criar conta",
+        "edit" => "Editar conta",
+        "save" => "Salvar conta",
+        "firstname" => "Nome",
+        "lastname" => "Sobrenome",
+        "general" => "Geral",
+        "changePassword" => "Mudar senha",
+        "oldPassword" => "Senha antiga",
+        "newPassword" => "Nova senha",
+        "retypePassword" => "Repita a nova senha",
+        "message" => [
+            'save' => "As informações da sua conta foram salvas com sucesso."
+        ]
+    ],
+    "scrap-image" => [
+        "title" => "Scrapping Images",
+        "search" => "Procurar"
+    ],
+    "error" => [
+        "lockTitle" => "Sua conta está bloqueada!",
+        "lockDescription" => "Sua conta está bloqueada pelo administrador agora, entre em contato com o administrador.",
+        "notMatchPassword" => "A senha especificada não corresponde à senha do banco de dados.",
+        "postImageValidate" => "The post image is not uploaded."
+    ],
+    "swal" => [
+        "delConfirm" => [
+            "title" => "Você tem certeza?",
+            "text" => "Você não seria capaz de reverter isso!"
+        ],
+    ]
 ];
