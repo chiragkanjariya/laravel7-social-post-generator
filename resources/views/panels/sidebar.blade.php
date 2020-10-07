a3893c07cf81259fc29602731830543537c5b84a
devdreamsolution <q3construction1@gmail.com>
2020-10-06 17:41:59 -0700

    remove logo text and toggle

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -8,15 +8,15 @@ $configData = Helper::applClasses();
     <ul class="nav navbar-nav flex-row">
       <li class="nav-item mr-auto"><a class="navbar-brand" href="dashboard-analytics">
           <div class="brand-logo"></div>
-          <h2 class="brand-text mb-0">Vuexy</h2>
+          {{-- <h2 class="brand-text mb-0">Vuexy</h2> --}}
         </a></li>
-      <li class="nav-item nav-toggle">
+      {{-- <li class="nav-item nav-toggle">
         <a class="nav-link modern-nav-toggle pr-0" data-toggle="collapse">
           <i class="feather icon-x d-block d-xl-none font-medium-4 primary toggle-icon"></i>
           <i class="toggle-icon feather icon-disc font-medium-4 d-none d-xl-block primary collapse-toggle-icon"
             data-ticon="icon-disc"></i>
         </a>
-      </li>
+      </li> --}}
     </ul>
   </div>
   <div class="shadow-bottom"></div>
