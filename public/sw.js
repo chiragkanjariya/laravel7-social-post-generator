4010f858facb3edac4e4d9c31a54c7fa223c6fa8
kcaribou <kcaribou0428@gmail.com>
2020-11-12 19:40:29 -0800

    notification issue fix

--- public/sw.js
+++ public/sw.js
@@ -7,6 +7,6 @@ self.addEventListener('notificationclick', function(event) {
   console.log(event.notification);
   event.notification.close();
   event.waitUntil(
-    clients.openWindow(event.notification.notification.data.url)
+    clients.openWindow(event.notification.data.url)
   );
 });
